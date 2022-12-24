4fd84e5496dfe5a25a64baff5dbca1751cd57475
mhmddorgham <mohammed.dorgham25@gmail.com>
2022-12-24 18:31:36 +0400

    Setup EJS and created First Template

--- app.js
+++ app.js
@@ -3,14 +3,41 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Its Holiday and index is " + today.getDay())
-  } else {
-    res.send("its Workday and index is " + today.getDay());
+  const theDate = new Date();
+  const currDay = theDate.getDay();
+  var today = ""
+
+  switch (currDay) {
+    case 0:
+      today = "Sunday";
+      break;
+    case 1:
+      today = "Monday";
+      break;
+    case 2:
+      today = "Tuesday";
+      break;
+    case 3:
+      today = "Wedensday";
+      break;
+    case 4:
+      today = "Thursday";
+      break;
+    case 5:
+      today = "Friday";
+      break;
+    case 6:
+      today = "Saturday";
+      break;
+    default:
+      console.log("Error, not day !");
+      break;
   }
+  res.render('index', { theDay: today });
 })
 
 app.listen(PORT, () => {
