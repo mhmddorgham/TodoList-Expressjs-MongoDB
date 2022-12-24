e3c375789fbf7d393db99875275d46bb2c439270
mhmddorgham <mohammed.dorgham25@gmail.com>
2022-12-24 22:18:56 +0400

    Setup addTask Function

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var newTasks = [];
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs');
 
@@ -9,36 +13,40 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
   const theDate = new Date();
   const currDay = theDate.getDay();
-  var today = ""
 
-  switch (currDay) {
-    case 0:
-      today = "Sunday";
-      break;
-    case 1:
-      today = "Monday";
-      break;
-    case 2:
-      today = "Tuesday";
-      break;
-    case 3:
-      today = "Wedensday";
-      break;
-    case 4:
-      today = "Thursday";
-      break;
-    case 5:
-      today = "Friday";
-      break;
-    case 6:
-      today = "Saturday";
-      break;
-    default:
-      console.log("Error, not day !");
-      break;
-  }
-  res.render('index', { theDay: today });
+  //Create data information the menstioned in the date:
+  var options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  console.log(theDate.toLocaleDateString("en-US", options)); // 9/17/2016
+
+  //set today to date that has been generated using tolocaleDateString:
+  today = theDate.toLocaleDateString("en-US", options);
+
+  res.render('index', { theDay: today, newItems: newTasks });
+
 })
+
+
+app.post("/", (req, res) => {
+  var newTask = req.body.task;
+  newTasks.push(newTask);
+
+  res.redirect("/");
+})
+
+/**
+ * app.post('/',(req,res)=>{
+  const {fname,lname,email,birthday} = req.body
+  res.render('submit',{first:fname,last:lname,email:email,birth:birthday})
+})
+ * 
+ * 
+ * 
+ */
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
